d7831ef8a291e1954af22e919b730c130073b172
smathot <s.mathot@cogsci.nl>
2021-06-30 17:04:27 +0200

    Update readme

---
+++ compile_readme.py
@@ -0,0 +1,17 @@
+# !coding=utf-8
+
+import re
+from academicmarkdown import build
+
+with open('readme-template.md') as fd:
+    text = fd.read()
+for m in re.finditer('```python(?P<code>.*?)```', text, re.DOTALL):
+    new_block = (
+        u'\n%--\npython: |\n'
+        + u'\n'.join([u' '+ s for s in m.group('code').strip().split(u'\n')])
+        + u'\n--%\n'
+    )
+    text = text.replace(m.group(0), new_block)
+md = build.MD(text)
+with open('readme.md', 'w') as fd:
+    fd.write(md)
